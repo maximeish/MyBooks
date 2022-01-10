e51d6c574d4333d977e2823969e1d9508dd4d07b
Maxime Ishimwe <mxmishimwe5@gmail.com>
2022-01-10 09:04:27 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { ChakraProvider, Box, VStack, Grid, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <VStack spacing={8}></VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+);
